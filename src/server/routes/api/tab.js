18085d98e8c355b4c931c5768cb499061a2642e8
Pranav Jain <pranajain@gmail.com>
2017-03-09 20:06:34 -0800

    Massive improvements to Tab capability Added ability to add/remove products to tab, get all tabs for merchant, get specific tab information for user, and specific tab info for merchant.

--- src/server/routes/api/tab.js
+++ src/server/routes/api/tab.js
@@ -9,6 +9,7 @@ var passport = require('../../lib/auth');
 var helpers = require('../../lib/helpers');
 var User = require('../../models/user');
 var Merchant = require('../../models/merchant');
+var Product = require('../../models/product.js');
 var redis = require('../../lib/redis');
 //(todo) using redis, create, edit and delete tabs
 //(todo) set up authentication for each merchant to access redis (NO ONE ELSE)
@@ -19,7 +20,7 @@ var redis = require('../../lib/redis');
  HMSET tab:MerchantID.UserID userID "<UserID>" merchantID "<MerchantID>" tabTotal <total tab money spent> Products "[{productID: \"product3\", name: \"product3\", time: { type: Date, default: Date.now }}]"
 
     (add to running list) so its searchable...
-    SADD tabs MerchantID.UserID
+    SADD tabs:MerchantID MerchantID.UserID
 
  (get)
  HMGET tab:MerchantID.UserID tabTotal merchantID Products etc.
@@ -80,14 +81,14 @@ var redis = require('../../lib/redis');
  */
 
 //open Tab localhost:8080/tabs/open/:id <- id is for Merchant
-router.post('/open/:id', helpers.ensureAuthenticated,
+router.post('/open', helpers.ensureAuthenticated,
     function(req, res, next) {
         //var store = new Store({
         //    'name': req.body.name,
         //    'description': req.body.description,
         //});
         var userID = req.user._id.toString();
-        var merchantID = req.params.id;
+        var merchantID = req.body.id;
         console.log("OPENING TAB WITH USER: " + userID + "\n and MERCHANT: " + merchantID);
         var tabKey = "tab:" + merchantID + "." + userID;
 
@@ -95,14 +96,21 @@ router.post('/open/:id', helpers.ensureAuthenticated,
         redis.hmset(tabKey, {
                 "userID": userID,
                 "merchantID": merchantID,
-                "tabTotal": 0,
+                "tabTotal": 0.0,
                 "numProducts": 0
 
             }, function(err, reply) {
             // reply is null when the key is missing
-                if(reply) {
+                if(err){
+                    return next(err);
+                }else {
                     console.log("Successfully opened tab!");
-                    res.status(200).send("success");
+                    res.status(200)
+                        .json({
+                            status: 'success',
+                            data: reply,
+                            message: 'Retrieved tab.'
+                        });
                 }
         });
 
@@ -127,47 +135,126 @@ router.post('/open/:id', helpers.ensureAuthenticated,
 //close tab
 
 //update tab add and remove product
-router.post('/update/add/:id', helpers.ensureAuthenticated,
+
+/*
+ tabs.MerchantID.userID1.Products.0.productID: ""
+ tabs.MerchantID.userID1.Products.0.time: "Date.now"
+ tabs.MerchantID.userID1.Products.1.productID: ""
+ tabs.MerchantID.userID1.Products.1.time: "Date.now"
+ */
+//add item to tab
+router.post('/update/add', helpers.ensureMerchantAuthenticated,
     function(req, res, next) {
         //var store = new Store({
         //    'name': req.body.name,
         //    'description': req.body.description,
         //});
-        var userID = req.user._id.toString();
-        var merchantID = req.params.id;
-        console.log("OPENING TAB WITH USER: " + userID + "\n and MERCHANT: " + merchantID);
+        var merchantID = req.user._id.toString();
+        var userID = req.body.id;
+        var productID = req.body.productID;
+
+        console.log("ADDING PRODUCT: " + productID + "\n TO TAB WITH USER: " + userID + "\n and MERCHANT: " + merchantID);
         var tabKey = "tab:" + merchantID + "." + userID;
 
 
-        redis.hmset(tabKey, {
-            "userID": userID,
-            "merchantID": merchantID,
-            "tabTotal": 0,
-            "numProducts": 0
+        redis.hget(tabKey, "numProducts", function (err, reply) {
+            var numProducts = parseInt(reply.toString());
+            var productIDKey = "Products." + numProducts + ".productID";
+            var productTimeKey = "Products." + numProducts + ".time";
+            var cost = 0.0;
+            var obj = {};
+            var date = new Date();
+            obj[productIDKey] = productID;
+            obj[productTimeKey] = date.toISOString();
+            //figure how to use q to do this better...
+            Product.findById(productID, function(err, product) {
+                if (err) {
+                    return next(err);
+                } else {
+                    cost = product.amount;
+                    //all the redis commands need to go into here.
+                    redis.multi().
+                    hincrby(tabKey, "numProducts", 1).
+                    HINCRBYFLOAT(tabKey, "tabTotal", cost).
+                    hmset(tabKey, obj).exec(
+                        function(err, reply) {
+                            // reply is null when the key is missing
+                            if(err){
+                                return next(err);
+                            }else{
+                                console.log("Successfully added to tab!");
+                                //test output
+                                redis.hgetall(tabKey, function (err, obj) {
+                                    console.dir(obj);
+                                    res.status(200)
+                                        .json({
+                                            status: 'success',
+                                            data: obj,
+                                            message: 'Retrieved tab.'
+                                        });
+                                });
+                            }
+                        });
+                }
+            });
 
-        }, function(err, reply) {
-            // reply is null when the key is missing
-            if(reply) {
-                console.log("Successfully opened tab!");
-                res.status(200).send("success");
-            }
         });
 
-        var tabsMember = merchantID + "." + userID;
-        var merchantTabs = "tabs:" + merchantID;
-        redis.sadd(merchantTabs, tabsMember, function(err, reply) {
-            if(reply){
-                console.log("Successfully added tabID to set.");
-            }
+
+    });
+
+//remove item from tab
+router.post('/update/remove', helpers.ensureMerchantAuthenticated,
+    function(req, res, next) {
+
+        var merchantID = req.user._id.toString();
+        var userID = req.body.id;
+
+        //this is the index number of the item we would like to remove from the tab
+        var itemNum = req.body.itemNum;
+
+        console.log("Removing PRODUCT in index: " + itemNum + "\n IN TAB WITH USER: " + userID + "\n and MERCHANT: " + merchantID);
+
+        var tabKey = "tab:" + merchantID + "." + userID;
+        var productIDKey = "Products." + itemNum + ".productID";
+        var productTimeKey = "Products." + itemNum + ".time";
+        redis.hget(tabKey, productIDKey, function (err, reply) {
+            var productID = reply;
+            var cost = 0.0;
+            //figure how to use q to do this better...
+            Product.findById(productID, function(err, product) {
+                if (err) {
+                    return next(err);
+                } else {
+                    cost = -1*product.amount;
+                    //decrement numproducts and tab total. get get rid of it from list.
+                    redis.multi().
+                    hincrby(tabKey, "numProducts", -1).
+                    HINCRBYFLOAT(tabKey, "tabTotal", cost).
+                    hdel(tabKey, productIDKey, productTimeKey)
+                        .exec(function(err, reply) {
+                            // reply is null when the key is missing
+                            if(err){
+                                return next(err);
+                            }else {
+                                console.log("Successfully removed from tab!");
+                                //test output
+                                redis.hgetall(tabKey, function (err, obj) {
+                                    console.dir(obj);
+                                    res.status(200)
+                                        .json({
+                                            status: 'success',
+                                            data: obj,
+                                            message: 'Retrieved tab.'
+                                        });
+                                });
+                            }
+                        });
+                }
+            });
+
         });
 
-        //test output
-        redis.hgetall(tabKey, function (err, obj) {
-            console.dir(obj);
-        });
-        redis.smembers(merchantTabs, function(err, reply) {
-            console.log(reply);
-        });
 
     });
 
@@ -179,8 +266,92 @@ keys   for index 0
 
 
 //get all tabs w/ merchantID
+//what information should we return?
+router.post('/getall', helpers.ensureMerchantAuthenticated,
+    function(req, res, next) {
 
-//get TabTotal w/ userID
+        var merchantID = req.user._id.toString();
+
+        //this is the index number of the item we would like to remove from the tab
+
+        console.log("Getting all tabs for MERCHANT: " + merchantID);
+
+        var tabsKey = "tabs:" + merchantID;
+        redis.smembers(tabsKey, function (err, reply) {
+            if(err){
+                return next(err);
+            }
+            else {
+                var allTabs = reply;
+                console.log("All tabs: " + allTabs);
+                res.status(200)
+                    .json({
+                        status: 'success',
+                        data: allTabs,
+                        message: 'Retrieved all tabs from merchant.'
+                    });
+            }
+        });
+
+
+    });
+
+
+//get tab info for user w/ userID like tabTotal and products bought
+//passing in merchantID for this specific user
+router.get('/tab/:id', helpers.ensureAuthenticated,
+    function(req, res, next) {
+
+        var userID = req.user._id.toString();
+        var merchantID = req.params.id;
+        //this is the index number of the item we would like to remove from the tab
+
+        var tabKey = "tab:" + merchantID + "." + userID;
+        //test output
+        redis.hgetall(tabKey, function (err, obj) {
+            if(err){
+                return next(err);
+            }else {
+                console.dir(obj);
+                res.status(200)
+                    .json({
+                        status: 'success',
+                        data: obj,
+                        message: 'Retrieved tab.'
+                    });
+            }
+        });
+
+
+    });
+
+//get User for merchant to populate information on bartender dashboard
+//passing in userID
+router.get('/user/:id', helpers.ensureMerchantAuthenticated,
+    function(req, res, next) {
+
+        var merchantID = req.user._id.toString();
+        var userID = req.params.id;
+        //this is the index number of the item we would like to remove from the tab
+
+        var tabKey = "tab:" + merchantID + "." + userID;
+        //test output
+        redis.hgetall(tabKey, function (err, obj) {
+            if(err){
+                return next(err);
+            }else {
+                console.dir(obj);
+                res.status(200)
+                    .json({
+                        status: 'success',
+                        data: obj,
+                        message: 'Retrieved tab.'
+                    });
+            }
+        });
+
+
+    });
 
 //find nearby bars (geolocationing)
 
