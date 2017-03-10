b82be6e74525fe8d634a3ea7e2bc6570efbfae21
Pranav Jain <pranajain@gmail.com>
2017-03-09 20:14:23 -0800

    Added UserType and refactored code.

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -5,61 +5,65 @@ var Schema = mongoose.Schema;
 var SALT_WORK_FACTOR;
 
 if (process.env.NODE_ENV === 'test') {
-  SALT_WORK_FACTOR = 1;
+    SALT_WORK_FACTOR = 1;
 } else {
-  SALT_WORK_FACTOR = 10;
+    SALT_WORK_FACTOR = 10;
 }
 
 var User = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true
-  },
-  products: [
-    {
-      productID: String,
-      name: String,
-      token: String,
-      time: { type: Date, default: Date.now }
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    products: [
+        {
+            productID: String,
+            name: String,
+            token: String,
+            time: { type: Date, default: Date.now }
+        }
+    ],
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    },
+    userType: {
+        type: String,
+        default: 'User'
+    },
+    stripe: {
+        type: String,
+        unique: true,
+        default: "NAN"
     }
-  ],
-  password: {
-    type: String,
-    required: true
-  },
-  admin: {
-    type: Boolean,
-    default: false
-  },
-  stripe: {
-    type: String,
-    unique: true,
-    default: "NAN"
-  }
 });
 
 User.methods.generateHash = function(password, callback) {
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(password, salt, function(err, hash) {
-      if (err) {
-        return next(err);
-      }
-      return callback(err, hash);
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+            return callback(err, hash);
+        });
     });
-  });
 };
 
 User.methods.comparePassword = function(password, done) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    if (err) {
-      return done(err);
-    }
-    return done(null, isMatch);
-  });
+    bcrypt.compare(password, this.password, function(err, isMatch) {
+        if (err) {
+            return done(err);
+        }
+        return done(null, isMatch);
+    });
 };
 
 
